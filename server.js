afad0a05a5dad1c4741795717a9251ab08ba0d23
Tom <72255127+thomasgardner4PU@users.noreply.github.com>
2021-12-20 13:47:47 +0000

    Version 4 - Working on API
    editing file structure

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 let express = require("express")
 const {engine} = require("express-handlebars");
+let mongoose = require('mongoose');
+require('dotenv/config');
+
+
+let indexRouter = require('./routes/index')
 
 let app = express()
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-    res.render('index');
-})
+app.use('/', indexRouter)
+
+//routes
+
+// app.get('/', function (req, res) {
+//     res.render('index');
+// })
 
 app.get('/profile', function (req, res){
     res.render('profile');
@@ -54,8 +64,29 @@ app.get('/api/projects', function (req, res){
     ])
 })
 
+// app.post('/api/projects', function (req, res){
+//     res.send([
+//         {
+//             projectId: 2,
+//             projectTitle: "COMP3008",
+//             projectDescription: "this is a test project",
+//             projectActivities: [
+//                 "Coming soon",
+//                 "Assignment Notes"
+//             ],
+//         }
+//     ])
+// })
+
 const path = require("path");
 app.use(express.static(path.join(__dirname, "./public")));
 
-app.listen(8000)
+// connection to database
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true },
+    () => console.log('connected to db'))
+
+app.listen( process.env.PORT || 8000)
 
