fd40b3993683a8a21381c5246118bf430f5a3163
tom <thomas.gardner-1@studnets.plymouth.ac.uk>
2021-12-29 17:43:43 +0000

    Version 4.5 - Implemented Search Functionality
    gets value based on HTML element using querySelector that when using the search filter and the user selects an option from the list they for example get 'recent projects' it only displays the most recent projects based on the p tag element in the project section inside the index.handlebars file

--- public/script.js
+++ public/script.js
@@ -1,10 +1,6 @@
-// global.document = new JSDOM(html).window.document;
-// const jsdom = require('jsdom');
-// const {JSDOM} = jsdom;
-
 window.onload = function() {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let myChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
             labels: [
@@ -24,37 +20,36 @@ window.onload = function() {
             }]
         },
     });
+
+    //search project
+
+    const project = document.querySelectorAll('.project')
+
+    const displayProject = (values) =>{
+        project.forEach(element =>{
+            element.style.display = "none"
+            const title = element.querySelector('h2').innerHTML.toUpperCase();
+            const status = element.dataset.status.toUpperCase();
+            const recent = element.querySelector('p').innerHTML.toUpperCase();
+
+            if (title.includes(values)){
+                element.style.display = "block"
+            }
+            else if (values !== 'RECENT' && status.includes(values)){
+                element.style.display = "block"
+            }
+            else if (values === 'RECENT' && recent.includes('HOUR') || recent.includes('DAY')){
+                element.style.display = "block"
+            }
+        })
+    }
+
+    searchProject.addEventListener('input', (e) =>{ // add's event to the 'searchProject' id where there is an input
+        // console.log(e) - this can be used to see if the event is working
+        displayProject(e.target.value.toUpperCase())
+    })
+
+    projectFilter.addEventListener('input', (e) =>{
+        displayProject(e.target.value.toUpperCase())
+    })
 }
-
-
-
-
-
-// const project = document.querySelectorAll('.project')
-//
-// const displayProject = (values) =>{
-//     project.forEach(element =>{
-//         element.style.display = "none"
-//         const title = element.querySelector('h2').innerHTML.toUpperCase();
-//         const status = element.dataset.status.toUpperCase();
-//         const recent = element.querySelector('p').innerHTML.toUpperCase();
-//
-//         if (title.includes(values)){
-//             element.style.display = "block"
-//         }
-//         else if(values != 'RECENT' && status.includes(values)){
-//             element.style.display = "block"
-//         }
-//         else if(values === 'RECENT' && recent.includes('HOUR') || recent.includes('RECENT')){
-//             element.style.display = "block"
-//         }
-//     })
-// }
-//
-// searchProject.addEventListener('input', (e) =>{
-//     displayProject(e.target.value.toUpperCase())
-// })
-//
-// projectFilter.addEventListener('input', (e) =>{
-//     displayProject(e.target.value.toUpperCase())
-// })
