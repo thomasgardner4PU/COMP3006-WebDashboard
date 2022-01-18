4c69826b53172b57babd59ad15c1823cf233ce8d
Tom <72255127+thomasgardner4PU@users.noreply.github.com>
2022-01-18 15:37:51 +0000

    Version 4.9
    Create button when clicked now opens popup form, however popup form needs to be edited for more text inputs

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -5,35 +5,35 @@ let router = express.Router();
 //api call goes here
 
 // create a data schema
-const projectSchema = {
-    projectId: parseInt,
-    projectTitle: String,
-    projectStatus: String,
-    projectUserfname: String,
-    projectUserlname: String,
-    projectActivities: [
-        String,
-        String
-    ]
-}
-
-const project = mongoose.model("project", projectSchema);
-
-router.get("/api/projects", function (req, res){
-    res.sendFile(__dirname + "/index");
-})
-
-router.post("/api/projects", function (req, res){
-    let newProject = new project({
-        projectId: req.body.projectId,
-        projectTitle: req.body.projectTitle,
-        projectStatus: req.body.projectStatus,
-        projectUserfname: req.body.projectUserfname,
-        projectUserlname: req.body.projectUserlname,
-        projectActivities: req.body.projectActivities
-    })
-    newProject.save();
-})
+// const projectSchema = {
+//     projectId: Int32,
+//     projectTitle: String,
+//     projectStatus: String,
+//     projectUserfname: String,
+//     projectUserlname: String,
+//     projectActivities: [
+//         String,
+//         String
+//     ]
+// }
+//
+// const project = mongoose.model("project", projectSchema);
+//
+// router.get("/api/projects", function (req, res){
+//     res.sendFile(__dirname + "/index");
+// })
+//
+// router.post("/api/projects", function (req, res){
+//     let newProject = new project({
+//         projectId: req.body.projectId,
+//         projectTitle: req.body.projectTitle,
+//         projectStatus: req.body.projectStatus,
+//         projectUserfname: req.body.projectUserfname,
+//         projectUserlname: req.body.projectUserlname,
+//         projectActivities: req.body.projectActivities
+//     })
+//     newProject.save();
+// })
 
 
 // router.get('/api/projects', function (req, res){
